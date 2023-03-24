98eb428e2e4a4660f638ea0d5956ff990b862ad6
Rauli Laine <rauli.laine@iki.fi>
2023-03-24 20:02:45 +0200

    Add little more test cases again

--- src/lexer/stack.test.ts
+++ src/lexer/stack.test.ts
@@ -1,15 +1,58 @@
 import { Stack } from "./stack";
 
 describe("class Stack", () => {
-  it("should throw an exception if attempting to remove item from empty stack", () => {
-    const stack = new Stack();
+  describe("empty()", () => {
+    it("should return `true` when the stack is empty", () =>
+      expect(new Stack().empty()).toBe(true));
 
-    expect(() => stack.pop()).toThrowError();
+    it("should return `false` when the stack is not empty", () => {
+      const stack = new Stack();
+
+      stack.push("test");
+
+      expect(stack.empty()).toBe(false);
+    });
   });
 
-  it("should throw an exception if attempting to access item from empty stack", () => {
-    const stack = new Stack();
+  describe("push()", () => {
+    it("should insert elements into the stack", () => {
+      const stack = new Stack();
 
-    expect(() => stack.top()).toThrowError();
+      stack.push("foo");
+      stack.push("bar");
+
+      expect(stack).toHaveProperty(["elements", 0], "foo");
+      expect(stack).toHaveProperty(["elements", 1], "bar");
+    });
+  });
+
+  describe("pop()", () => {
+    it("should throw error if the stack is empty", () =>
+      expect(() => new Stack().pop()).toThrow());
+
+    it("should remove and return the top element from the stack", () => {
+      const stack = new Stack();
+
+      stack.push("foo");
+      stack.push("bar");
+
+      expect(stack.pop()).toBe("bar");
+      expect(stack).toHaveProperty(["elements", "length"], 1);
+    });
+  });
+
+  describe("top()", () => {
+    it("should throw error if the stack is empty", () =>
+      expect(() => new Stack().top()).toThrow());
+
+    it("should return the top element from the stack", () => {
+      const stack = new Stack();
+
+      stack.push("foo");
+      stack.push("bar");
+
+      expect(stack.top()).toBe("bar");
+      expect(stack).toHaveProperty(["elements", "length"], 2);
+    });
   });
 });
